73c83809bd80c7fada8ff7bfcffd8ff80582fd02
alamchichi <ayoub.lamchichi.info@gmail.com>
2023-09-12 16:17:42 +0200

    classe abstraite

---
+++ Maitre.java
@@ -0,0 +1,8 @@
+public class Maitre extends Interieur{
+
+    private static final int heuresDues = 192;
+
+    public Maitre() {
+        super(heuresDues);
+    }
+}
---
+++ Prag.java
@@ -0,0 +1,9 @@
+public class Prag extends Interieur{
+
+    private static final int heuresDues = 384;
+
+    public Prag(){
+        super(heuresDues);
+
+    }
+}
